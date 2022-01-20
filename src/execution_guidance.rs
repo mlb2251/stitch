6365fa003978f83d70dea6fb740086ec821819d0
Matt Bowers <mattb6503@gmail.com>
2022-01-20 13:16:47 -0500

    first draft of execution_guided_compression() is done, needs debugging

--- src/execution_guidance.rs
+++ src/execution_guidance.rs
@@ -23,19 +23,57 @@ pub fn execution_guided_compression<D: Domain>(
     let inputs: Vec<Envs<D>> = tasks.iter().map(|t| t.inputs.clone()).collect();
 
     let roots: Vec<Id> = programs.expr.nodes[usize::from(programs.expr.root())].children().iter().copied().collect();
+    let N: usize = programs.expr.root().into();
 
 
-    // execute the programs on their inputs to build up a semantic set at each node.
+    // execute the programs on their inputs to build up evalresults at each node.
     for (root_id,envs) in roots.iter().zip(inputs.iter()) {
         for env in envs.iter() {
             programs.eval_child_safe(*root_id, env).unwrap();
         }
     }
 
-    let cvecs: Vec<EvalResults<D>> = (0..programs.expr.root().into()).map(|i| programs.evals_of_node(i.into())).collect();
+    let evalresults: Vec<EvalResults<D>> = (0..N).map(|i| programs.evals_of_node(i.into())).collect();
     let free_vars: Vec<HashSet<i32>> = programs.expr.free_vars(false);
+    // separate out the two parts of evalresults
+    let envs: Vec<Envs<D>> = evalresults.iter().map(|er| er.iter().map(|(env,_)| env.clone()).collect()).collect();
+    let results: Vec<Vec<Val<D>>> = evalresults.iter().map(|er| er.iter().map(|(_,v)| v.clone()).collect()).collect();
 
-    
+    assert!(N == evalresults.len() && N == free_vars.len());
+
+
+    // filter down these evalresults to throw out any variables in the contexts that arent actually FVs in the expr itself (bc we're clearly invariant to these). Freeze these semantic sets.
+    // for (evalresult,fvs) in evalresults.iter_mut().zip(free_vars.iter()) {
+    //     for (env,_res) in evalresult {
+    //         env.retain(|i| fvs.contains(i));
+    //     }
+    // }
+
+    let mut rewrites: Vec<Vec<Id>> = vec![vec![]; N];
+
+    // Now lets go full N^2 (not just N*(N-1)) and pick i,j to see if `i <- j` rewrite is valid
+    for i in 0..N {
+        for j in 0..N {
+            // check if i <- j is valid, so i.fvs must be a subset of j.fvs. Note that
+            // it's more precise to use free_vars here than to use the env vecs because much
+            // of the env might never be used.
+            if !free_vars[i].is_subset(&free_vars[j]) {
+                continue;
+            }
+            // execute j in each env of i, exiting early if you ever come across a crash or a differing result
+            for (env,ires) in evalresults[i].iter() {
+                match programs.eval_child_safe(j.into(), env) {
+                    Ok(jres) => if jres != *ires { continue } // if result differs i <- j is not valid
+                    Err(_) => continue, // if error then i <- j is not valid
+                }
+            }
+            rewrites[i].push(j.into()); // successfully found a rewrite that works!
+        }
+    }
+
+    println!("done! found {} rewrites over {} nodes", rewrites.iter().map(|r| r.len()).sum::<usize>(), N);
+
+
 
 
 
