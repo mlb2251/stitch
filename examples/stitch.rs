c6784b027cdce67620091e72bef3429b9cc8b338
Matt Bowers <mattb6503@gmail.com>
2022-03-10 21:15:59 -0500

    building template works, need to write actual bindings

---
+++ examples/stitch.rs
@@ -0,0 +1,20 @@
+use stitch::*;
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+
+
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn stitch(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+
+    Ok(())
+}
