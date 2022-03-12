2c92c01dc9d0abcfc446f07352bc44dff27b0643
Matt Bowers <mlbowers@mit.edu>
2022-03-12 18:59:51 -0500

    (wip) Python bindings (#59)
    * building template works, need to write actual bindings
    
    * bindings mostly done
    
    * finished bindings v1

---
+++ examples/stitch.rs
@@ -0,0 +1,96 @@
+use stitch::*;
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use serde_json::json;
+
+
+/// Calls compression.rs::compression(), and has a similar API to bin/compress.rs
+/// `programs` should be a list of program strings. Keyword arguments exist for
+/// all the parameters of CompressionStepConfig (eg max_arity etc). `iterations` controls
+/// the number of inventions that are returned.
+/// Returns: a json string similar to the output of bin/compress.rs with some minor changes.
+/// You can parse this string with `import json; json.loads(output)`.
+#[pyfunction(
+    programs,
+    "*",
+    iterations = "3",
+    max_arity = "2",
+    threads = "1",
+    inv_candidates = "1",
+    fifo_worklist = "false",
+    ascending_worklist = "false",
+    lossy_candidates = "false",
+    no_cache = "false",
+    show_rewritten = "false",
+    no_opt_free_vars = "false",
+    no_opt_single_use = "false",
+    no_opt_upper_bound = "false",
+    no_opt_force_multiuse = "false",
+    no_opt_useless_abstract = "false",
+    no_stats = "false",
+)]
+fn compression(
+    py: Python,
+    programs: Vec<String>,
+    iterations: usize,
+    max_arity: usize,
+    threads: usize,
+    inv_candidates: usize,
+    fifo_worklist: bool,
+    ascending_worklist: bool,
+    lossy_candidates: bool,
+    no_cache: bool,
+    show_rewritten: bool,
+    no_opt_free_vars: bool,
+    no_opt_single_use: bool,
+    no_opt_upper_bound: bool,
+    no_opt_force_multiuse: bool,
+    no_opt_useless_abstract: bool,
+    no_stats: bool) -> String {
+
+    let cfg = CompressionStepConfig {
+        max_arity,
+        threads,
+        inv_candidates,
+        fifo_worklist,
+        ascending_worklist,
+        lossy_candidates,
+        no_cache,
+        show_rewritten,
+        no_opt_free_vars,
+        no_opt_single_use,
+        no_opt_upper_bound,
+        no_opt_force_multiuse,
+        no_opt_useless_abstract,
+        no_stats,
+    };
+
+    let programs: Vec<Expr> = programs.iter().map(|p| p.parse().unwrap()).collect();
+    programs_info(&programs);
+    let programs: Expr = Expr::programs(programs);
+
+    // release the GIL and call compression
+    let step_results = py.allow_threads(||
+        stitch::compression(&programs, iterations, &cfg)
+    );
+
+    let out = json!({
+        "cfg": cfg,
+        "iterations": iterations,
+        "original_cost": programs.cost(),
+        "original": programs.split_programs().iter().map(|p| p.to_string()).collect::<Vec<String>>(),
+        "invs": step_results.iter().map(|inv| inv.json()).collect::<Vec<serde_json::Value>>(),
+    });
+
+    // return as something you could json.loads(out) from in python
+    out.to_string()
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn stitch(_py: Python, m: &PyModule) -> PyResult<()> {
+    // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    // m.add_function(wrap_pyfunction!(soot, m)?)?;
+    m.add_function(wrap_pyfunction!(compression, m)?)?;
+    Ok(())
+}
