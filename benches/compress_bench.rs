3e22f991703f082b36b5d3b9092e2cb9c8d3fbd9
Matt Bowers <mattb6503@gmail.com>
2022-03-12 18:30:16 -0500

    migrated `compression()` into `compression.rs` and separated out json-saving from that

--- benches/compress_bench.rs
+++ benches/compress_bench.rs
@@ -27,13 +27,13 @@ pub fn compression_benchmark(c: &mut Criterion) {
 
     // run benchmarks
     let tstart = std::time::Instant::now();
-    group.bench_function("cogsci/nuts-bolts.json -i3 -a2 single-threaded", |b| b.iter(|| compression(black_box(&programs_logo_train_200), black_box(&cfg_arity2_t1), black_box(3))));
-    group.bench_function("logo/train_200.json -t4 -i3 -a2", |b| b.iter(|| compression(black_box(&programs_logo_train_200), black_box(&cfg_arity2), black_box(3))));
-    group.bench_function("cogsci/train_200.json -t4 -i3 -a3", |b| b.iter(|| compression(black_box(&programs_logo_train_200), black_box(&cfg_arity3), black_box(3))));
-    group.bench_function("cogsci/nuts-bolts.json -t4 -i3 -a2", |b| b.iter(|| compression(black_box(&programs_nuts_bolts), black_box(&cfg_arity2), black_box(3))));
-    group.bench_function("cogsci/nuts-bolts.json -t4 -i3 -a3", |b| b.iter(|| compression(black_box(&programs_nuts_bolts), black_box(&cfg_arity3), black_box(3))));
-    group.bench_function("cogsci/dials.json -t4 -i1 -a2", |b| b.iter(|| compression(black_box(&programs_dials), black_box(&cfg_arity2), black_box(1))));
-    group.bench_function("cogsci/furniture.json -t4 -i1 -a2", |b| b.iter(|| compression(black_box(&programs_furniture), black_box(&cfg_arity2), black_box(1))));
+    group.bench_function("cogsci/nuts-bolts.json -i3 -a2 single-threaded", |b| b.iter(|| compression(black_box(&programs_logo_train_200), black_box(3), black_box(&cfg_arity2_t1))));
+    group.bench_function("logo/train_200.json -t4 -i3 -a2", |b| b.iter(|| compression(black_box(&programs_logo_train_200), black_box(3), black_box(&cfg_arity2))));
+    group.bench_function("cogsci/train_200.json -t4 -i3 -a3", |b| b.iter(|| compression(black_box(&programs_logo_train_200), black_box(3), black_box(&cfg_arity3))));
+    group.bench_function("cogsci/nuts-bolts.json -t4 -i3 -a2", |b| b.iter(|| compression(black_box(&programs_nuts_bolts), black_box(3), black_box(&cfg_arity2))));
+    group.bench_function("cogsci/nuts-bolts.json -t4 -i3 -a3", |b| b.iter(|| compression(black_box(&programs_nuts_bolts), black_box(3), black_box(&cfg_arity3))));
+    group.bench_function("cogsci/dials.json -t4 -i1 -a2", |b| b.iter(|| compression(black_box(&programs_dials), black_box(1), black_box(&cfg_arity2))));
+    group.bench_function("cogsci/furniture.json -t4 -i1 -a2", |b| b.iter(|| compression(black_box(&programs_furniture), black_box(1), black_box(&cfg_arity2))));
     group.finish();
     println!("Total `cargo bench` time: {}s", tstart.elapsed().as_secs());
     println!("{}/target/criterion/compression/report/index.html", std::env::current_dir().unwrap().to_str().unwrap())
@@ -41,36 +41,3 @@ pub fn compression_benchmark(c: &mut Criterion) {
 
 criterion_group!(benches, compression_benchmark);
 criterion_main!(benches);
-
-
-
-fn compression(
-    programs_expr: &Expr,
-    cfg: &CompressionStepConfig,
-    iterations: usize,
-) -> Vec<CompressionStepResult> {
-    let mut rewritten: Expr = programs_expr.clone();
-    let mut step_results: Vec<CompressionStepResult> = Default::default();
-
-    for _ in 0..iterations {
-        let inv_name = format!("inv{}",step_results.len());
-
-        // call actual compression
-        let res: Vec<CompressionStepResult> = compression_step(
-            &rewritten,
-            &inv_name,
-            cfg,
-            &step_results);
-
-        if !res.is_empty() {
-            // rewrite with the invention
-            let res: CompressionStepResult = res[0].clone();
-            rewritten = res.rewritten.clone();
-            step_results.push(res);
-        } else {
-            break;
-        }
-    }
-
-    step_results
-}
