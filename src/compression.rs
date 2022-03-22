mlb2251/stitch

commit e0805a2b45561c615b150e550a1719e31984ae87

fix: inventions should never be rooted to the left of an App as this implies a function type as the return type

src/compression.rs
@@ -476,6 +476,11 @@ fn get_appzippers(treenodes: &[Id], no_cache:bool, egraph: &mut EGraph, cfg: &Co
                     let new: AppZipper = x_appzipper.clone_prepend(ZNode::Arg,Some(f));
                     appzippers.push(new);
                 }
+
+                // this for eta long form / dreamcoder compatability: no appzipper bodies can be rooted to the left of an App
+                // because that means the body is a function type, which isnt allowed. For example an arity 2 invention with a
+                // function type body would be effectively arity 3 and dreamcoder doesnt support this sort of thing.
+                all_appzippers.get_mut(&f).unwrap().clear();
             },
             Lambda::Lam([b]) => {
                 let ref b_appzippers = all_appzippers[&b];

commit 71baee42ed585a03c3ef5a283f80da66d57906eb

FIX: e0805a2 broke things briefly and this fixes it

src/compression.rs
@@ -477,10 +477,6 @@ fn get_appzippers(treenodes: &[Id], no_cache:bool, egraph: &mut EGraph, cfg: &Co
                     appzippers.push(new);
                 }
 
-                // this for eta long form / dreamcoder compatability: no appzipper bodies can be rooted to the left of an App
-                // because that means the body is a function type, which isnt allowed. For example an arity 2 invention with a
-                // function type body would be effectively arity 3 and dreamcoder doesnt support this sort of thing.
-                all_appzippers.get_mut(&f).unwrap().clear();
             },
             Lambda::Lam([b]) => {
                 let ref b_appzippers = all_appzippers[&b];
@@ -520,6 +516,15 @@ fn get_appzippers(treenodes: &[Id], no_cache:bool, egraph: &mut EGraph, cfg: &Co
             },
         }
         all_appzippers.insert(*treenode, appzippers);
+    }
+
+    for treenode in treenodes.iter() {
+        if let Lambda::App([f,_]) = egraph[*treenode].nodes[0] {
+            // this for eta long form / dreamcoder compatability: no appzipper bodies can be rooted to the left of an App
+            // because that means the body is a function type, which isnt allowed. For example an arity 2 invention with a
+            // function type body would be effectively arity 3 and dreamcoder doesnt support this sort of thing.
+            all_appzippers.get_mut(&f).unwrap().clear();
+        }
     }
 
     // note that we must be very careful pruning here. Most pruning isnt allowed, for example you cant prune things

src/util.rs
@@ -177,9 +177,10 @@ pub fn dc_inv_str(inv: &Invention, past_step_results: &Vec<CompressionStepResult
     // add the "#" that dreamcoder wants and change lam -> lambda
     let mut res: String = format!("#{}", body);
     res = res.replace("(lam ", "(lambda ");
-    // inline any past inventions using their dc_inv_str
+    // inline any past inventions using their dc_inv_str. Match on "fn_i)" and "fn_i " to avoid matching fn_1 on fn_10 or any other prefix
     for past_step_result in past_step_results.iter() {
-        res = res.replace(past_step_result.inv.name.as_str(), past_step_result.dc_inv_str.as_str());
+        res = res.replace(&format!("{})",past_step_result.inv.name), &format!("{})",past_step_result.dc_inv_str));
+        res = res.replace(&format!("{} ",past_step_result.inv.name), &format!("{} ", past_step_result.dc_inv_str));
     }
     res
 }