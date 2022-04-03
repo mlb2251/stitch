b37926b3c31aca1da9c899c9a5cd48a665194612
Matt Bowers <mlbowers@mit.edu>
2022-04-02 22:03:47 -0400

    bench_dreamcoder added

---
+++ experiments/bench_dreamcoder.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+if [ -z $COMPRESSION_BIN ]
+then
+    echo "Please set environment variable \$COMPRESSION_BIN to the ocaml compression binary" 
+    exit 1
+fi
+
+if [ -z $1 ]
+then
+    echo "Usage: ./bench_dreamcoder.sh benches/bench_name"
+    exit 1
+fi
+
+BENCH_DIR=$1
+OUT_DIR="${BENCH_DIR}/out/dc/$(TZ='America/New_York' date '+%Y-%m-%d_%H-%M-%S')"
+
+mkdir -p $OUT_DIR/raw
+mkdir -p $OUT_DIR/stderr
+
+# run Dreamcoder on all the input files from the benchmark
+for BENCH_PATH in $BENCH_DIR/bench*.json; do
+    BENCH=$(basename -s .json $BENCH_PATH)
+    echo "[bench_dreamcoder.sh] Running Dreamcoder with on: $BENCH"
+
+    /usr/bin/time -v $COMPRESSION_BIN $BENCH_PATH > $OUT_DIR/raw/$BENCH.json 2> >(tee $OUT_DIR/stderr/$BENCH.stderr >&2)
+done
+
+python analyze.py process dc $OUT_DIR
+
+echo "Done: $OUT_DIR"
+
