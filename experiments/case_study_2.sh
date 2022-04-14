71f079477dfb65ba75d6e8709a5586be8b94e65a
theoxo <theoxo@mit.edu>
2022-04-14 16:59:33 -0400

    impr: no verbose print in latency sensitive run

--- experiments/case_study_2.sh
+++ experiments/case_study_2.sh
@@ -8,7 +8,7 @@ then
     exit 1
 fi
 
-OUT_DIR="cs2/$(TZ='America/New_York' date '+%Y-%m-%d_%H-%M-%S')"
+OUT_DIR="cs2/quiet/$(TZ='America/New_York' date '+%Y-%m-%d_%H-%M-%S')"
 mkdir -p $OUT_DIR
 
 # Save some info about what state of the repo this experiment was run in
@@ -33,11 +33,12 @@ for WL_PATH in $STITCH_DIR/data/cogsci/*.json; do
     python3 split_data.py $SPLIT $SEED $WL_PATH "$WL-$SPLIT-$SEED-split.json"
     echo "[case_study_2.sh] Split data; seed used was $SEED, train test % was $SPLIT"
     echo "Running Stitch"
-    /usr/bin/time -v $STITCH_DIR/target/release/compress "$WL-$SPLIT-$SEED-split.json" --hole-choice=last --heap-choice=max-bound --verbose-best --fmt=split-programs-list --split-train-test --max-arity=3 --iterations=1 --no-mismatch-check --out=$OUT_DIR/$WL/$SPLIT/$SEED.json > $OUT_DIR/$WL/$SPLIT/$SEED.stderrandout 2>&1 &
+    #/usr/bin/time -v $STITCH_DIR/target/release/compress "$WL-$SPLIT-$SEED-split.json" --hole-choice=last --heap-choice=max-bound --verbose-best --fmt=split-programs-list --split-train-test --max-arity=3 --iterations=1 --no-mismatch-check --out=$OUT_DIR/$WL/$SPLIT/$SEED.json > $OUT_DIR/$WL/$SPLIT/$SEED.stderrandout 2>&1 &
+    /usr/bin/time -v $STITCH_DIR/target/release/compress "$WL-$SPLIT-$SEED-split.json" --hole-choice=last --heap-choice=max-bound --fmt=split-programs-list --split-train-test --max-arity=3 --iterations=1 --no-mismatch-check --out=$OUT_DIR/$WL/$SPLIT/$SEED.json > $OUT_DIR/$WL/$SPLIT/$SEED.stderrandout 2>&1 &
     done
     wait  # move this up/down between loops to change how many jobs to run at once
-    done
     rm -v *-split.json
+    done
 done
 
 
