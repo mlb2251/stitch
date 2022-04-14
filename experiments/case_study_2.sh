e4112af80310b8cf0c16a02426ca6d9299bc9eb3
theoxo <theoxo@mit.edu>
2022-04-14 15:36:35 -0400

    feat: measure peak memory usage

--- experiments/case_study_2.sh
+++ experiments/case_study_2.sh
@@ -27,13 +27,13 @@ popd
 for WL_PATH in $STITCH_DIR/data/cogsci/*.json; do
     WL=$(basename -s .json $WL_PATH)
     for SPLIT in 5 10 15 20 30 40 50 65 80 95 ; do
+    for SEED in {1..50} ; do
     mkdir -p $OUT_DIR/$WL/$SPLIT
-    for SEED in 01 18 09 24 10 25 11 29 1999 2001 ; do
     echo "[case_study_2.sh] Starting workload $WL, split $SPLIT, seed $SEED"
     python3 split_data.py $SPLIT $SEED $WL_PATH "$WL-$SPLIT-$SEED-split.json"
     echo "[case_study_2.sh] Split data; seed used was $SEED, train test % was $SPLIT"
     echo "Running Stitch"
-    $STITCH_DIR/target/release/compress "$WL-$SPLIT-$SEED-split.json" --verbose-best --fmt=split-programs-list --split-train-test --max-arity=3 --iterations=1 --no-mismatch-check --out=$OUT_DIR/$WL/$SPLIT/$SEED.json > $OUT_DIR/$WL/$SPLIT/$SEED.stderrandout 2>&1 &
+    /usr/bin/time -v $STITCH_DIR/target/release/compress "$WL-$SPLIT-$SEED-split.json" --hole-choice=last --heap-choice=max-bound --verbose-best --fmt=split-programs-list --split-train-test --max-arity=3 --iterations=1 --no-mismatch-check --out=$OUT_DIR/$WL/$SPLIT/$SEED.json > $OUT_DIR/$WL/$SPLIT/$SEED.stderrandout 2>&1 &
     done
     wait  # move this up/down between loops to change how many jobs to run at once
     done
