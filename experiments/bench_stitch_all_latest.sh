779a06edcd5381258624a3f43ccf9f211a085174
Matt Bowers <mlbowers@mit.edu>
2022-06-29 14:48:59 -0400

    switch to min() and other changes to cs1

--- experiments/bench_stitch_all_latest.sh
+++ experiments/bench_stitch_all_latest.sh
@@ -1,5 +1,14 @@
 
-for COMPARE_TO in $(python analyze.py latest dreamcoder); do
+
+if [ -z $1 ]
+then
+    echo "Usage: ./bench_stitch_all_latest.sh benches/"
+    exit 1
+fi
+
+BENCHES=$1
+
+for COMPARE_TO in $(python analyze.py latest dreamcoder $BENCHES); do
     ./bench_stitch.sh compare $COMPARE_TO
 done
 
--- experiments/process_all.sh
+++ experiments/process_all.sh
@@ -4,3 +4,8 @@ for f in benches/*/out/dc/* ; do
     # echo $f
     python3 analyze.py process dreamcoder $f
 done
+
+for f in benches/*/out/stitch/* ; do
+    # echo $f
+    python3 analyze.py process stitch $f
+done
