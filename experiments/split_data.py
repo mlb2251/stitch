6385136d379695eea95e4c4fbf219b32b27f4b92
theoxo <theoxo@mit.edu>
2022-07-05 11:30:51 -0400

    feat: claim-2 exps scripts

--- experiments/split_data.py
+++ experiments/split_data.py
@@ -2,10 +2,9 @@ import json
 import sys
 import random
 
-split = int(sys.argv[1])
-seed  = int(sys.argv[2])
-dataf = sys.argv[3]
-outf  = sys.argv[4]
+seed  = int(sys.argv[1])
+dataf = sys.argv[2]
+outf  = sys.argv[3]
 
 random.seed(seed)
 
@@ -15,18 +14,10 @@ with open(dataf, 'rb') as data:
 
     random.shuffle(programs)
 
-    # split is % of programs that should be put
-    # into the training set.
-    # Since we have 250 programs per input file,
-    # we need to multiply this by 2.5 to get
-    # the number of programs to put in the
-    # training set.
-    test_split = 250 - int(20*2.5)  # Test set size is fixed to 20% of programs
-    train_split = int(split * float(test_split)/100.)
+    split = 200  # Test set size is fixed to 20% of programs
 
-    assert train_split <= test_split  # no overlap of train/test allowed
-    train_progs = programs[:train_split]
-    test_progs = programs[:test_split]
+    train_progs = programs[:split]
+    test_progs = programs[split:]
 
     with open(outf, 'w') as out:
         json.dump([train_progs, test_progs], out)
