a298785153aaddb20c328cea82a2de29793e3124
theoxo <theoxo@mit.edu>
2022-10-04 10:51:09 -0400

    feat: scripts for easier reproducability

--- experiments/ablation.sh
+++ experiments/ablation.sh
@@ -8,7 +8,7 @@ then
     exit 1
 fi
 
-OUT_DIR="cogsci/out/stitch/$(TZ='America/New_York' date '+%Y-%m-%d_%H-%M-%S')"
+OUT_DIR="ablation-results/$(TZ='America/New_York' date '+%Y-%m-%d_%H-%M-%S')"
 
 # compile Stitch
 pushd $STITCH_DIR
@@ -16,53 +16,26 @@ cargo build --release --bin=compress
 popd
 
 mkdir -p $OUT_DIR/raw
-mkdir -p $OUT_DIR/stderr
+mkdir -p $OUT_DIR/stdout
 
-NON_TOWER_DOMAINS=( "dials" "furniture" "nuts-bolts" "wheels" )
-# single ablation experiments
+# Save some info about what state of the repo this experiment was run in
+# to aid reporducibility
+echo -n "Current git commit: " > $OUT_DIR/readme.md
+git log -n 1 >> $OUT_DIR/readme.md
+echo -n "Current git branch: " >> $OUT_DIR/readme.md
+git rev-parse --abbrev-ref HEAD >> $OUT_DIR/readme.md
+STITCH_FLAGS="--max-arity=3 --iterations=1"
+echo -n "Stitch hyperparameter settings: " >> $OUT_DIR/readme.md
+echo $STITCH_FLAGS >> $OUT_DIR/readme.md
+
 for WL_PATH in $STITCH_DIR/data/cogsci/*.json; do
-#for WL in "castle" "city" "house" ; do
     WL=$(basename -s .json $WL_PATH)
-    #WL_PATH=${STITCH_DIR}/data/cogsci/${WL}.json
     echo "[ablation.sh] Starting workload $WL"
-    #for FVO in "--no-opt-free-vars" "" ; do
-    #OPTIMS=( "" "--no-opt-single-use" "--no-opt-upper-bound" "--no-opt-force-multiuse" "--no-opt-arity-zero")
-    #if [[ "${NON_TOWER_DOMAINS[*]}" =~ "${WL}" ]]; then
-    #    # workload is not a tower, add UAO as optim to consider
-    #    OPTIMS+=( "--no-opt-useless-abstract" )
-    #fi
-
-    for OPTIM in "--no-opt" ; do
+    # In the lingo of the paper, "--no-opt-useless-abstract" is "no-arg-capture";
+    # "--no-opt-force-multiuse" is "no-redundant-args".
+    for OPTIM in "" "--no-opt-upper-bound" "--no-opt-force-multiuse" "--no-opt-useless-abstract" "--no-opt" ; do
         echo "Running with OPTIM=$OPTIM"
-        $STITCH_DIR/target/release/compress $WL_PATH $OPTIM --max-arity=3 --iterations=1 --no-mismatch-check --out=$OUT_DIR/raw/${WL}${OPTIM}.json > $OUT_DIR/stderr/${WL}${OPTIM}.stderr 2>&1 &
+        timeout 90m /usr/bin/time -v $STITCH_DIR/target/release/compress $WL_PATH $OPTIM $STITCH_FLAGS --out=$OUT_DIR/raw/${WL}${OPTIM}.json > $OUT_DIR/stdout/${WL}${OPTIM}.stdout 2>&1 || true #&
     done
 done
-wait $(jobs -rp)
-
-# ablation combination experiments
-#for WL_PATH in $STITCH_DIR/data/cogsci/*.json; do
-#    WL=$(basename -s .json $WL_PATH)
-#    echo "[ablation.sh] Starting workload $WL"
-#    #for FVO in "--no-opt-free-vars" "" ; do
-#    for FVO in "" ; do
-#    for SUO in "--no-opt-single-use" "" ; do
-#    for STO in "--no-opt-single-task" ; do
-#    for UBO in "--no-opt-upper-bound" "" ; do
-#    for MUO in "--no-opt-force-multiuse" "" ; do
-#    #for UAO in "--no-opt-useless-abstract" "" ; do
-#    for UAO in "" ; do
-#    for AZO in "--no-opt-arity-zero" "" ; do
-#        echo "Running with FVO=$FVO, SUO=$SUO, UBO=$UBO, MUO=$MUO, UAO=$UAO, AZO=$AZO"
-#        $STITCH_DIR/target/release/compress $WL_PATH $FVO $SUO $UBO $MUO $UAO $AZO --verbose-best --max-arity=3 --iterations=1 --no-mismatch-check --out=$OUT_DIR/raw/$WL-$FVO-$SUO-$UBO-$MUO-$UAO-$AZO.json > $OUT_DIR/stderr/$WL-$FVO-$SUO-$UBO-$MUO-$UAO-$AZO.stderr2>&1 &
-#    done
-#    wait $(jobs -rp)
-#    done
-#    done
-#    done
-#    done
-#    done
-#    done
-#done
-
-
-echo "Done: $OUT_DIR"
+echo "Done, wrote results to $OUT_DIR"
---
+++ experiments/plot.py
@@ -0,0 +1,159 @@
+import sys
+import os
+import re
+
+import matplotlib.pyplot as plt 
+import numpy as np
+from prettytable import PrettyTable
+
+import seaborn as sns
+sns.set_theme(color_codes=True)
+sns.set(font_scale=2)
+
+def get_stats(path):
+    with open(path, 'r') as infile:
+        ss = {}
+        for line in infile:
+            if line.strip():
+                ss[str(re.match(r'^([^:]*):', line).group(1))] = int(re.match(r'.*Median: ([^$]*)$', line).group(1))
+        return ss
+
+def get_data(path, mode):
+    if mode == 'claim-2':
+        with open(path, 'r') as infile:
+            for line in infile:
+                if line.startswith('Cost Improvement: '):
+                    train_ratio = float(re.match(r'Cost Improvement: \(([^x]*)x.*', line).group(1))
+                if line.startswith('Test set compression with all inventions applied: '):
+                    test_ratio = float(re.match(r'Test set compression with all inventions applied: (.*)', line).group(1))
+                elif line.startswith('Time: '):
+                    s = float(re.match(r'Time: ([^m]*)ms.*', line).group(1))/1000.
+                elif 'Maximum resident set size (kbytes):' in line:
+                    mb = float(re.match(r'.*Maximum resident set size \(kbytes\): (.*)', line).group(1))/1000.
+            return (train_ratio, test_ratio, s, mb)
+
+
+    elif mode == 'claim-3':
+        with open(path, 'r') as infile:
+            steps = [0]
+            ratios = [1.0]
+            total_steps = None 
+            for line in infile:
+                if '[new best utility]' in line:
+                    step  = re.match(r'.*step=([^ ]*).*', line).group(1)
+                    ratio = re.match(r'.*trainratio=([^ ]*).*', line).group(1)
+                    steps.append(int(step) + 1)  # they're zero indexed, so add 1
+                    ratios.append(float(ratio))
+                elif 'Stats {' in line:
+                    total_steps = int(re.match(r'.*worklist_steps: ([^,]*),.*', line).group(1))
+            return (total_steps, steps, ratios)
+
+    elif mode == 'ablation':
+
+        MAX_TIME = 60 * 90  # max number of seconds of runtime to accept for a run
+
+        # First get the baseline performance
+        with open(path + '.stdout', 'r') as basef:
+            s = basef.read()
+            base_time  = float(re.search(r'User time \(seconds\): ([^\n]*)\n', s).group(1))
+            assert base_time <= MAX_TIME and "bytes failed" not in s,  "baseline timed out or ran out of RAM"
+            base_steps = int(re.search(r'.*worklist_steps: ([^,]*),.*', s).group(1))
+
+        results = {}
+        opts = ("--no-opt-useless-abstract", "--no-opt-force-multiuse", "--no-opt-upper-bound", "--no-opt")
+        for opt in opts:
+            with open(path + opt + '.stdout', 'r') as f:
+                s = f.read()
+                time_match = re.search(r'.*User time \(seconds\): ([^\n]*)\n', s)
+                if time_match is None or float(time_match.group(1)) > MAX_TIME:
+                    results[opt] = "TIME"
+                elif "bytes failed" in s:
+                    results[opt] = "MEM"
+                else:
+                    steps = int(re.search(r'.*worklist_steps: ([^,]*),.*', s).group(1))
+                    results[opt] = "{:.2f}x".format(float(steps)/float(base_steps))
+
+        return results
+
+
+
+mode = sys.argv[1]
+path = sys.argv[2]
+
+assert mode in ['claim-2', 'claim-3', 'ablation']
+
+
+plt.rcParams.update({'font.size': 22})
+#workloads = [wl for wl in os.listdir(path) if wl != 'readme.md']
+workloads = ['nuts-bolts', 'bridge', 'dials', 'city', 'furniture', 'castle', 'wheels', 'house']
+wl_to_human_readable = {
+    'nuts-bolts': 'nuts & bolts',
+    'bridge': 'bridges',
+    'dials': 'gadgets',
+    'city': 'cities',
+    'furniture': 'furniture',
+    'castle': 'castles',
+    'wheels': 'vehicles',
+    'house': 'houses',
+}
+if mode == 'claim-2':
+    for wl in workloads:
+        seeds = list(range(1, 51))  #os.listdir('/'.join([path, wl]))
+        train_ratios = []
+        test_ratios = []
+        runtimes = []
+        mem_usages = []
+        for seed in seeds:
+            train_ratio, test_ratio, seconds, mb = get_data('/'.join([path, wl, f'{seed}.stderrandout']), mode)
+            train_ratios.append(train_ratio)
+            test_ratios.append(test_ratio)
+            runtimes.append(seconds)
+            mem_usages.append(mb)
+        train_r_stats = (np.mean(train_ratios), np.std(train_ratios))
+        test_r_stats = (np.mean(test_ratios), np.std(test_ratios))
+        runtime_stats = (np.mean(runtimes), np.std(runtimes))
+        mem_stats = (np.mean(mem_usages), np.std(mem_usages))
+        print(f'{wl_to_human_readable[wl]}: \n\ttrain_r_stats={train_r_stats}\n\ttest_r_stats={test_r_stats}\n\truntime_stats={runtime_stats}\n\tmem_stats={mem_stats}')
+
+elif mode == 'claim-3':
+    fig = plt.figure(figsize=(10,10))
+    markers = ['o', '8', 's', 'p', 'P', '*', 'h', 'D']
+    for wl, marker in zip(workloads, markers):
+        infile = [f for f in os.listdir('/'.join([path, wl])) if f.endswith('.stderrandout')]
+        assert len(infile) == 1
+        infile = infile[0]
+        total_steps, steps, ratios = get_data('/'.join([path, wl, infile]), 'ex3')
+
+        # make da plot yo
+        steps = [100. * s / total_steps for s in steps]
+        ratios = [100. * (r - 1.0) / (ratios[-1] - 1.0) for r in ratios]
+        ax = plt.step(steps, ratios, marker=marker, where='post', label=wl_to_human_readable[wl], ms=10)
+    plt.xscale('log')
+    plt.legend(loc='best')
+    plt.xlim((0.0001, 100))
+    xticks = [0.001, 0.01, 0.1, 1.0, 10, 100]
+    plt.xticks(xticks, [f'{t}%' for t in xticks])
+    yticks = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]#[50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+    plt.yticks(yticks, [f'{t}%' for t in yticks])
+    plt.grid(True, which='minor', alpha=0.5)
+    plt.xlabel('Search progress (log scale; %)')
+    plt.ylabel('Reduction in size thus far vs optimal reduction (%)')
+    fig.tight_layout()
+    plt.savefig(f"cs2-ex3.pdf")
+
+elif mode == 'ablation':
+
+    # Reorder workloads to mimic the table in the paper
+    workloads = ['bridge', 'castle', 'city', 'dials', 'furniture', 'house', 'nuts-bolts', 'wheels']
+    pt = PrettyTable(['Ablation'] + [wl_to_human_readable[wl] for wl in workloads])
+    res_all = {}
+    for wl in workloads:
+        res = get_data(path + '/stdout/' + wl, mode)
+        for opt in res:
+            if opt not in res_all:
+                res_all[opt] = []
+            res_all[opt].append(res[opt])
+    pt.add_rows([[k] + res_all[k] for k in sorted(res_all, reverse=True)])
+    print(pt)
+
+
