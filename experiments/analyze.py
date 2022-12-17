37d2249c9219fe348d6ceb3128af269e5854aa25
Matthew Bowers <mlbowers@mit.edu>
2022-12-17 05:10:45 -0500

    tweak

--- experiments/analyze.py
+++ experiments/analyze.py
@@ -712,7 +712,7 @@ if __name__ == '__main__':
             'wheels': 'vehicles',
             'house': 'houses',
         }
-        domains = ['dials','nuts-bolts', 'furniture', 'wheels', 'bridge', 'city', 'castle', 'house']
+        domains = ['nuts-bolts', 'dials', 'furniture', 'wheels', 'bridge', 'city', 'castle', 'house']
 
         table = PrettyTable(['Domain', 'Training set C.R.', 'Test set C.R.', 'Runtime (s)', 'Peak mem. usage (MB)'])
 
