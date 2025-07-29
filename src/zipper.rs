840f7bcb20af8ac99549e0da7b0ca1ccc0301781
Kavi Gupta <kavi@berkeley.edu>
2025-07-29 11:45:43 -0700

    inline always

--- src/zipper.rs
+++ src/zipper.rs
@@ -6,42 +6,42 @@ pub struct Zipper(pub Vec<ZNode>);
 
 impl Zipper {
 
-    #[inline]
+    #[inline(always)]
     pub fn iter(&self) -> impl Iterator<Item = &ZNode> {
         self.0.iter()
     }
     
-    #[inline]
+    #[inline(always)]
     pub fn ends_with_func(&self) -> bool {
         matches!(self.0.last(), Some(ZNode::Func))
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn function_arity(&self) -> usize {
         self.0.iter().rev().take_while(|znode| **znode == ZNode::Func).count()
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn depth_root_to_arg(&self) -> usize {
         self.0.iter().filter(|x| **x == ZNode::Body).count()
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn starts_with(&self, other: &Zipper) -> bool {
         self.0.starts_with(&other.0)
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn add_to_front(&mut self, node: ZNode) {
         self.0.insert(0, node);
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn add_to_end(&mut self, node: ZNode) {
         self.0.push(node);
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn remove_from_end(&mut self) {
         self.0.pop();
     }
